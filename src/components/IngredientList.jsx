bffc621ae2ca3ea1faa2aeab4a20602f24f17106
MellGriffin <missmelissagriffin@gmail.com>
2024-08-14 16:21:31 -0500

    Getting some code in App

---
+++ src/components/IngredientList.jsx
@@ -0,0 +1,13 @@
+// src/components/IngredientList.jsx
+const IngredientList = (props) => {
+    return (
+        <ul>
+        {props.ingredients.map((ingredient, index) => {
+          return <li key={index}>{ingredient}</li>;
+        })}
+      </ul>
+    )
+  };
+  
+  export default IngredientList;
+  
